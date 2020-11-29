2e78d2a78e97094c73f04b382bf1c66c4e187c66
Anson <ansfoong@gmail.com>
2020-11-29 14:17:26 -0500

    React, Typescript, Webpack Configuration

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+import { User } from "./components/User";
+export default function App() {
+  return (
+    <div>
+      <h1>Hello, World!!</h1>
+      <User />
+    </div>
+  );
+}
